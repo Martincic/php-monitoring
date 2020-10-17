46ada7e260a77d035366b3b59b45050fa1021ab8
Martincic <martincic.tomas@gmail.com>
2020-10-17 12:52:57 +0200

    database transfered to class

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
    </body>
 </html>
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
    	#echo shell_exec("python /var/www/martincic-dev/assets/buzzer.py 2>&1");
 	include('assets/save.php');
 ?>
