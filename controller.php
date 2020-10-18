b7db1a3364f5ba2c03deff7df1c70107f2f14967
Martincic <martincic.tomas@gmail.com>
2020-10-18 10:18:52 +0200

    additional security factor

--- controller.php
+++ controller.php
@@ -1,21 +1,23 @@
 <?php
 include('autoload.php');
 include('assets/include-bootstrap.php');
-
-if($_SERVER['REQUEST_METHOD'] == "POST") 
+if($_SERVER['REMOTE_ADDR'] == $_SERVER['ADDR']) 
 {
-    if(($_POST['username'] == env('username')) && ($_POST['password'] == env('password'))) 
+    if($_SERVER['REQUEST_METHOD'] == "POST") 
     {
-        $key = env('key');
-        echo "
-            <form action='switch.php' method='post' class='w-50 mt-5 mx-auto'>
-                <div class='form-group'>
-                <input id='key' name='key' value='${key}' hidden>
-                <button type='submit' class='btn btn-primary btn-lg btn-block'>SWITCH LIGHTS</button>
-            </form>
-        ";
+        if(($_POST['username'] == env('username')) && ($_POST['password'] == env('password'))) 
+        {
+            $key = env('key');
+            echo "
+                <form action='switch.php' method='post' class='w-50 mt-5 mx-auto'>
+                    <div class='form-group'>
+                    <input id='key' name='key' value='${key}' hidden>
+                    <button type='submit' class='btn btn-primary btn-lg btn-block'>SWITCH LIGHTS</button>
+                </form>
+            ";
 
 
-    } else die("nothing here");
-}
-else die("nothing here");
+        } else die("nothing here");
+    }
+    else die("nothing here");
+}else die("ur not home");
