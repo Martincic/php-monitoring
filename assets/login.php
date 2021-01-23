59f0810dcae08b13d804785c2ed8c83a16703634
Martincic <martincic.tomas@gmail.com>
2021-01-23 12:19:31 +0100

    bug fixed

--- assets/login.php
+++ assets/login.php
@@ -76,12 +76,12 @@
 <script>
 var slider = document.getElementById("temp");
 var output = document.getElementById("odabranaTemp");
-output.innerHTML = slider.value; // Display the default slider value
+// output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
-// }
+Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
   var request = new XMLHttpRequest()
 
