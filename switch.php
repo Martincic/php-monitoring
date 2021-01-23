eee758578f5f37a6cf334cd09ba6b7bebf57f674
root <root@martincic.dev>
2021-01-23 13:27:44 +0100

    cleanup

--- switch.php
+++ switch.php
@@ -6,19 +6,15 @@ error_reporting(E_ALL);
 
 //You can also report all errors by using -1
 error_reporting(-1);
-echo 0;
 
 //If you are feeling old school
 ini_set('error_reporting', E_ALL);
 
 if($_SERVER['REQUEST_METHOD'] == "POST") 
 {
-    echo 1;
     if($_POST['key'] == env('key')) 
     {
-        echo 2;
         if(isset($_POST['range'])) {
-            echo 3;
             $temp=$_POST['range'];
             $status = shell_exec("python3 settemp.py ".$temp);
             //echo "Temp namjestena na ". $temp. " stupnja. STATUS: ".$status;
@@ -39,9 +35,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
         shell_exec("gpio -g write ". $num." ".$state);   
         
         //loop back here
-        echo 3.5;
         include('assets/login.php');
-        echo 4;
         include('assets/saveLogin.php');
     }
 }
